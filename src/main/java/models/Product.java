dc70b0561e2d35e25246f305a70abe18e3ba19fc
Konrad <konrad.dawydiuk@gmail.com>
2019-03-10 08:40:19 +0100

    change in Product model

--- src/main/java/models/Product.java
+++ src/main/java/models/Product.java
@@ -1,21 +1,31 @@
 package models;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 /**
  * Created by Judith on 09.12.2018.
  */
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Getter
+@Setter
+@Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String name;
+
+    @Transient
     private Employee foundryEmployee;
+
     private LocalDateTime foundryDate;
 
 
