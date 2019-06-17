b8cd2c0dad84942fc918b6289a9a24eb213a52d1
Konrad <konrad.dawydiuk@gmail.com>
2019-06-17 20:31:07 +0200

    add JWT impl

---
+++ src/main/java/models/User.java
@@ -0,0 +1,14 @@
+package models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    private String login;
+    private String password;
+}
