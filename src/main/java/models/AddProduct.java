d07617758360b01dd84f2d3ea311fb733dba3db6
Konrad <konrad.dawydiuk@gmail.com>
2019-11-03 10:58:36 +0100

    repair gradle build

--- src/main/java/models/AddProduct.java
+++ src/main/java/models/AddProduct.java
@@ -1,11 +1,13 @@
 package models;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import models.enums.ProductType;
 
 @Setter
 @Getter
+@Builder
 public class AddProduct {
 
     private int number;
