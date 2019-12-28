eab4c635660755c69131d977ae7248820c188dbc
Konrad <konrad.dawydiuk@gmail.com>
2019-12-28 08:24:05 +0100

    add fields to User

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,14 +1,40 @@
 package models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 6)
     private String login;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 6)
     private String password;
+
+    @Email
+    private String email;
 }
